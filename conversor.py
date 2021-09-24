51f328b3c00ed36d9693b32a9e0259c78fa0b984
MiiikAnd <oliveira.mike1@gmail.com>
2021-09-23 21:54:50 -0300

    rage quit

--- conversor.py
+++ conversor.py
@@ -1,18 +1,34 @@
 import tkinter as tk
 
 
+
 class Application(tk.Frame):
+
+
+
+
     def __init__(self, master=None):
         super().__init__(master)
         self.quit = tk.Button(self, text="Fechar", fg="red",
                               command=self.master.destroy)
+        self.executar = tk.Button(self, text="Executar")
+        #:terminar essa porcaria#
+        self.diretorio = tk.Entry(self, width=20, xscrollcommand=1)
         self.master = master
         self.grid()
         self.create_widgets()
 
     def create_widgets(self):
+        self.executar.grid(row=2, column=1)
+        self.executar["command"] = self.acao_botao(self.diretorio)
+        self.quit.grid(row=2, column=3)
+        self.diretorio.grid(row=1, column=1)
 
-        self.quit.grid(row=10, column=2)
+    def acao_botao(texto):
+        texto = self.diretorio.get()
+        print(texto)
+
+
 
 
 root = tk.Tk()
---
+++ teste.py
@@ -0,0 +1,4 @@
+texto = open("Teste1.txt", 'r')
+print(texto.read())
+texto.close()
+
